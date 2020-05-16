156c7429383a11101a387452e98c31b2c778d5b1
sparkchaser <1392511+sparkchaser@users.noreply.github.com>
2020-05-15 23:21:31 -0500

    Add "export to CSV" functionality to the guild roster

--- Viewmodels/RosterViewmodel.cs
+++ Viewmodels/RosterViewmodel.cs
@@ -1,6 +1,8 @@
 ﻿using goh_ui.Models;
 using goh_ui.Views;
+using Microsoft.Win32;
 using System;
+using System.IO;
 using System.Windows;
 
 namespace goh_ui.Viewmodels
@@ -14,6 +16,8 @@ namespace goh_ui.Viewmodels
             Members = members ?? throw new ArgumentNullException("members");
 
             GuildName = Guild.name;
+
+            Export = new SimpleCommand(ExportTable);
         }
 
 
@@ -29,6 +33,44 @@ namespace goh_ui.Viewmodels
         public GuildInfo Guild { get; private set; }
 
         #endregion
+
+        public SimpleCommand Export { get; private set; }
+
+        /// <summary> Export the currently-displayed table data to a CSV file. </summary>
+        private void ExportTable()
+        {
+            // Prompt user for output filename
+            var dlg = new SaveFileDialog()
+            {
+                Title = "Export to file",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                OverwritePrompt = true,
+                DefaultExt = ".csv",
+                Filter = "CSV Files|*.csv|All Files|*.*"
+            };
+            var result = dlg.ShowDialog();
+            if (!result.HasValue || result == false || string.IsNullOrWhiteSpace(dlg.FileName))
+            {
+                return;
+            }
+
+            // Generate CSV version of table
+            try
+            {
+                using (var writer = new StreamWriter(dlg.FileName))
+                {
+                    writer.WriteLine("Name,Total Power,Character Power,Meaningful Power");
+                    foreach (var p in Members)
+                    {
+                        writer.WriteLine($"{p.Name},{p.Power},{p.CharacterPower},{p.MeaningfulPower}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error saving file:\n{e.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
 
         /// <summary> Show detailed information about a specific player. </summary>
